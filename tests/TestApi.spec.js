2ced51bcd908b4bdb90ca99fa9e13e2d320dd859
priyesh suman <priyesh.suman@gmail.com>
2026-03-15 22:29:08 -0400

    commented test for aji schema

--- tests/TestApi.spec.js
+++ tests/TestApi.spec.js
@@ -1,5 +1,5 @@
 import {test , expect} from '@playwright/test';
-import Ajv from 'ajv';
+//import Ajv from 'ajv';
 // test('Post Call API', async ({request}) => {    
 // const response =  await request.get(
 //     "https://api.api-ninjas.com/v1/cryptoprice?symbol=LTCBTC",
@@ -30,46 +30,46 @@ test('Get Call API', async ({ request }) => {
 
 });
 
-test('Schema Validation - Get Call API', async ({ request }) => {
-  const response = await request.get(
-    "https://api.api-ninjas.com/v1/cryptoprice?symbol=LTCBTC",
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'x-api-key': 'nWQg9AxqJ9mw0otvuUi2KK3hKZnG48bvBPeEJQGW'
-      }
-    }
-  );
-  const responseBody = await response.json();
-  console.log('========================');
-  console.log(responseBody);
-  console.log('========================');
+// test('Schema Validation - Get Call API', async ({ request }) => {
+//   const response = await request.get(
+//     "https://api.api-ninjas.com/v1/cryptoprice?symbol=LTCBTC",
+//     {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//         'x-api-key': 'nWQg9AxqJ9mw0otvuUi2KK3hKZnG48bvBPeEJQGW'
+//       }
+//     }
+//   );
+//   const responseBody = await response.json();
+//   console.log('========================');
+//   console.log(responseBody);
+//   console.log('========================');
   
-  const schema = {
-    type: "object",
-    properties: {
-      symbol: { type: "string" },
-      price: { type: "string"},
-      timestamp: { type: "number" },
+//   const schema = {
+//     type: "object",
+//     properties: {
+//       symbol: { type: "string" },
+//       price: { type: "string"},
+//       timestamp: { type: "number" },
       
-    },
-    required: ["symbol", "price","timestamp"]
+//     },
+//     required: ["symbol", "price","timestamp"]
       
-  };
+//   };
   
-  const ajv = new Ajv();
-  const validate = ajv.compile(schema);
-  const valid = validate(responseBody);
+//   const ajv = new Ajv();
+//   const validate = ajv.compile(schema);
+//   const valid = validate(responseBody);
   
-  if (!validate(responseBody)) {
-  console.log(validate.errors);
-}
+//   if (!validate(responseBody)) {
+//   console.log(validate.errors);
+// }
   
-  expect(valid).toBe(true);
+//   expect(valid).toBe(true);
 
 
-});
+// });
 
 
 
